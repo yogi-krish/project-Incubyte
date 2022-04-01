cc2e778f0ec87e9a2a82b6adb5c0000ff5956bea
51150 <51150@192.168.1.5>
2022-04-01 13:58:00 +0530

    Refactored Code

--- incubyte-StringCalculator/src/com/yogi/incubyte/StringCalculator.java
+++ incubyte-StringCalculator/src/com/yogi/incubyte/StringCalculator.java
@@ -11,22 +11,11 @@ public class StringCalculator {
 		if(numbers.startsWith("//")) {
 			String delimiter=numbers.charAt(2)+"";
 			String numbers1=numbers.substring(4, numbers.length());
-			String[] numbersArray=numbers1.split(delimiter);
-			int sum=0;
-			for(String num:numbersArray) {
-				sum += to_Int(num);
-			}
-			
-			return sum;
+			return helper(numbers1, delimiter+"|\n");
 		}
 		
 		else if(numbers.contains(",")) {
-			String[] numbersArray = numbers.split(",|\n");
-			int sum=0;
-			for(String num:numbersArray) {
-				sum += to_Int(num);
-			}
-			return sum;
+			return helper(numbers, ",|\n");
 		}
 		
 		else {
@@ -38,5 +27,15 @@ public class StringCalculator {
 
 	public static int to_Int(String str) {
 		return Integer.parseInt(str);
+	}
+	
+	public static int helper(String numbers, String delimiter) {
+		
+		String[] numbersArray = numbers.split(delimiter);
+		int sum=0;
+		for(String num:numbersArray)
+			sum += to_Int(num);
+		
+		return sum;
 	}
 }
